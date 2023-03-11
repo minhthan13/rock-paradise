95cb96bcaca1244ca0db74cf80db6091b20e9a95
Krantz <nguyenminhthan1308@gmail.com>
2023-03-11 13:11:15 +0700

    merge

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="footer">
     <div class="item">
-        <div class="title-footer">Shop</div>
+        <div class="title-footer">Rock Paradise</div>
         <ul>
             <li>Gift Cards</li>
             <li>Sitemap</li>
@@ -8,23 +8,6 @@
             <li>Esty United Kingdom</li>
             <li>Esty Germany</li>
             <li>Esty Canada</li>
-        </ul>
-    </div>
-    <div class="item">
-        <div class="title-footer">Shop</div>
-        <ul>
-            <li>Gift Cards</li>
-            <li>Sitemap</li>
-            <li>Esty Blog</li>
-            <li>Esty United Kingdom</li>
-
-        </ul>
-    </div>
-    <div class="item">
-        <div class="title-footer">Shop</div>
-        <ul>
-            <li>Gift Cards</li>
-            <li>Sitemap</li>
         </ul>
         <div class="dow-app">
             <p>Dowload the Esty App</p>
@@ -35,5 +18,11 @@
             <i class="fa-brands fa-twitter"></i>
             <i class="fa-brands fa-youtube"></i>
         </div>
+    </div>
+    <div class="item">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.113557644751!2d106.71392131471866!3d10.802613892303738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175296157af55a9%3A0xb7b4c46904d7158a!2zMzYvNSBOZ3V54buFbiBHaWEgVHLDrSwgUGjGsOG7nW5nIDI1LCBCw6xuaCBUaOG6oW5oLCBUaMOgbmggcGjhu5EgSOG7kyBDaMOtIE1pbmgsIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1678460028908!5m2!1svi!2s"
+            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 </div>
--- resources/views/menu/contact.blade.php
+++ resources/views/menu/contact.blade.php
@@ -1,4 +1,44 @@
 @extends('layouts.app')
 @section('main')
-    <div>Contact</div>
+    <div class="container-contact">
+        <div class="contact-text">
+            <h2 class="title-product">GET IN TOUCH</h2>
+            <p>
+                <strong>Email</strong>
+            </p>
+            <p>sales@rockparadise.com</p>
+            <p>
+                <strong>Phone</strong>
+            </p>
+            <p>866-364-9777</p>
+        </div>
+        <div class="form-contact">
+            <form action="">
+                <h2 class="title-product">Contact Us</h2>
+                <div class="input-text">
+                    <div class="name-email">
+                        <div class="name">
+                            <label for="">Name</label>
+                            <input type="text">
+                        </div>
+                        <div class="email">
+                            <label for="">Email</label>
+                            <input type="email">
+                        </div>
+                    </div>
+                    <div class="phone">
+                        <label for="">Phone</label>
+                        <input type="number" name="" id="">
+                    </div>
+                    <div class="message">
+                        <label for="">Message</label>
+                        <textarea name="" id="" cols="30" rows="10"></textarea>
+                    </div>
+                </div>
+                <div class="btn">
+                    <button type="submit">SEND</button>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
