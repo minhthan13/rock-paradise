67f09a22c2505c6e8afaf44b66c07d8df3371f70
Krantz <nguyenminhthan1308@gmail.com>
2023-03-14 22:35:02 +0700

    request size & rate in filter

--- app/Http/Controllers/FilterController.php
+++ app/Http/Controllers/FilterController.php
@@ -8,71 +8,49 @@ use Illuminate\Support\Facades\DB;
 class FilterController extends Controller
 {
     public function filter(Request $request, $view){
-        switch($view){
-            case 'products':{
-                $query = DB::table('product')
+        $query = DB::table('product')
                 ->join('image','product.product_id','=','image.product_id')
                 ->where('image.default_image','=','1');
-                $priceMin = $request->input('price_min', 1);
-                $priceMax = $request->input('price_max', 40000);
-                if($priceMin==0){return redirect('/products');}
-                $query->whereBetween('price', [$priceMin, $priceMax]);
-                $rates = $request->input('rate', []);
-                if (!empty($rates)) {
-                    $query->where('rate','<=', $rates);
-                }
-                if (!empty($brands)) {
-                    $query->whereIn('brand', $brands);
-                }
-                $colors = $request->input('color', []);
-                if (!empty($colors)) {
-                    $query->whereIn('color', $colors);
-                }
-                $sizes = $request->input('size', []);
-                if (!empty($sizes)) {
-                    $query->whereIn('size', $sizes);
-                }
-                $query->orderBy("product.product_id",'ASC');
+        // xử lý query cho bộ lọc
+        $priceMin = $request->input('price_min', 1);
+        $priceMax = $request->input('price_max', 40000);
+        if($priceMin==0){return redirect('/products');}
+        $query->whereBetween('price', [$priceMin, $priceMax]);
+        $rates = $request->input('rate', []);
+        if (!empty($rates)) {
+            $query->where('rate','<=', $rates);
+        }
+        $brands = $request->input('brand', []);
+        if (!empty($brands)) {
+            $query->whereIn('brand', $brands);
+        }
+        $colors = $request->input('color', []);
+        if (!empty($colors)) {
+            $query->whereIn('color', $colors);
+        }
+        $sizes = $request->input('size', []);
+        if (!empty($sizes)) {
+            $query->whereIn('size', $sizes);
+        }
+        $query->orderBy("product.product_id",'ASC');
+        // xử lý cho các view được gán componets
+        switch($view){
+            case 'products':{
                 $productsPerPage = $query->paginate(12, ['*'], 'page', $request->get('page'));
                 return view('products', ['products' =>$productsPerPage->appends(request()->query())]);
                 break;
             }
             case 'bestselling':{
-                $query = DB::table('product')
-                ->join('image','product.product_id','=','image.product_id')
-                ->where('image.default_image','=','1')
-                ->where('product.is_top','=','1');
-                $priceMin = $request->input('price_min', 1);
-                $priceMax = $request->input('price_max', 40000);
-                if($priceMin==0){return redirect('/products');}
-                $query->whereBetween('price', [$priceMin, $priceMax]);
-                $rates = $request->input('rate', []);
-                if (!empty($rates)) {
-                    $query->where('rate','<=', $rates);
-                }
-                if (!empty($brands)) {
-                    $query->whereIn('brand', $brands);
-                }
-                $colors = $request->input('color', []);
-                if (!empty($colors)) {
-                    $query->whereIn('color', $colors);
-                }
-                $sizes = $request->input('size', []);
-                if (!empty($sizes)) {
-                    $query->whereIn('size', $sizes);
-                }
-                    $query->orderBy("product.product_id",'ASC');
-                    $productsPerPage = $query->paginate(12, ['*'], 'page', $request->get('page'));
-                
+                $query->where('product.is_top','=','1');
+                $productsPerPage = $query->paginate(12, ['*'], 'page', $request->get('page'));
                 return view('menu.bestselling',['bsell'=>$productsPerPage->appends(request()->query())]);
                 break;
             }
-
+    
             default:{
                 return view('error');
                 break;
             }
-        }
-        
+        }        
     }
 }
